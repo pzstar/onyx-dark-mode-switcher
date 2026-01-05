d7d10bbdbe01da85bfd34304dc6e4a9ebe593857
Prabin Dangol <neprabin@gmail.com>
2026-01-05 21:44:14 +0545

    Update onyx-dark-mode-switcher.php

--- onyx-dark-mode-switcher.php
+++ onyx-dark-mode-switcher.php
@@ -7,7 +7,6 @@
  * Description:       Dark and Light Theme for your Wordpress website
  * Version:           1.0.0
  * Author:            HashThemes
- * Author URI:        https://hashthemes.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       onyx-dark-mode-switcher
